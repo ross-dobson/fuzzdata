a0def80ad42002e865de6ff87ea16993cc695cbf
pyoor <pyoor@users.noreply.github.com>
2019-03-13 09:05:08 -0400

    Add pref to enable @-moz-document rule

--- settings/firefox/prefs-default-e10s.js
+++ settings/firefox/prefs-default-e10s.js
@@ -144,6 +144,7 @@ user_pref("layout.accessiblecaret.enabled", true); // normally enabled on device
 user_pref("layout.css.column-span.enabled", true); // requested in bug 1491723
 user_pref("layout.css.font-variations.enabled", true);
 user_pref("layout.css.individual-transform.enabled", true); // requested in bug 1507647
+user_pref("layout.css.moz-document.content.enabled", true); // Enable @-moz-document
 user_pref("layout.css.ruby.enabled", true);
 user_pref("layout.css.vertical-text.enabled", true);
 user_pref("layout.debug.enable_data_xbl", true);
