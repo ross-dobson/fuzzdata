ae1284baf795c31af3ea9be514e863bd040eca43
pyoor <pyoor@users.noreply.github.com>
2019-04-18 16:20:21 -0400

    Re-enable offscreen canvas

--- settings/firefox/prefs-default-e10s.js
+++ settings/firefox/prefs-default-e10s.js
@@ -128,7 +128,7 @@ user_pref("gfx.color_management.mode", 1); // 0=off, 1=full, 2=tagged images onl
 user_pref("gfx.content.use-native-pushlayer", true);
 user_pref("gfx.downloadable_fonts.keep_variation_tables", true);
 user_pref("gfx.downloadable_fonts.otl_validation", false); // required for some demo fonts
-user_pref("gfx.offscreencanvas.enabled", false);
+user_pref("gfx.offscreencanvas.enabled", true);
 user_pref("gfx.prefer-mesa-llvmpipe", false);
 user_pref("html5.offmainthread", true);
 user_pref("image.animated.decode-on-demand.batch-size", 1);
