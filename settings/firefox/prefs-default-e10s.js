1b63fb2c6fbc1223bea83f22fe5ed15d81d40016
pyoor <pyoor@users.noreply.github.com>
2019-05-14 10:42:23 -0400

    Enable ResizeObserver

--- settings/firefox/prefs-default-e10s.js
+++ settings/firefox/prefs-default-e10s.js
@@ -145,6 +145,7 @@ user_pref("layout.css.column-span.enabled", true); // requested in bug 1491723
 user_pref("layout.css.font-variations.enabled", true);
 user_pref("layout.css.individual-transform.enabled", true); // requested in bug 1507647
 user_pref("layout.css.moz-document.content.enabled", true); // Enable @-moz-document
+user_pref("layout.css.resizeobserver.enabled", true); // Enable resize observer
 user_pref("layout.css.ruby.enabled", true);
 user_pref("layout.css.vertical-text.enabled", true);
 user_pref("layout.debug.enable_data_xbl", true);
