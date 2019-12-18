fe1c8901583e3661db8a988c0b3f36e1a0dc3bd2
Tyson Smith <tyson.w.smith@gmail.com>
2019-12-17 18:20:01 -0800

    [settings] Add javascript.options.parser_defer_allocation

--- settings/firefox/prefs-webrender-e10s.js
+++ settings/firefox/prefs-webrender-e10s.js
@@ -141,6 +141,7 @@ user_pref("image.webp.enabled", true);
 user_pref("enable dom.indexedDB.experimental", true);
 user_pref("javascript.options.blinterp", false);  // remove once bug 1599923 is fixed
 user_pref("javascript.options.gczeal", 0); // domfuzz
+user_pref("javascript.options.parser_defer_allocation", true);  // TODO: this should be randomly enabled
 user_pref("layers.acceleration.force-enabled", true);
 user_pref("layout.accessiblecaret.enabled", true); // normally enabled on devices with touchscreens (emilio)
 user_pref("layout.css.column-span.enabled", false); // requested in bug 1491723 (temporarily disabled)
